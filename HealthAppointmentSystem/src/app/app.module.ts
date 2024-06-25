f3577401f068cb62d1ecc87659c5fbad50170c4f
Umut Tölek <umuttolekk@gmail.com>
2024-06-25 23:08:52 +0300

    Styles changed

--- HealthAppointmentSystem/src/app/app.module.ts
+++ HealthAppointmentSystem/src/app/app.module.ts
@@ -1,37 +1,63 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SignInPageComponent } from './sign-in-page/sign-in-page.component';
-import { SignInPagePhotoComponent } from './sign-in-page-photo/sign-in-page-photo.component';
-import { SignInPageLoginComponent } from './sign-in-page-login/sign-in-page-login.component';
 import { AppointmentPageComponent } from './appointment-page/appointment-page.component';
-import { AppointmentPageHeaderComponent } from './appointment-page-header/appointment-page-header.component';
 import { AppointmentPageAppointmentsComponent } from './appointment-page-appointments/appointment-page-appointments.component';
-import { AppointmentPageNearestHospitalsComponent } from './appointment-page-nearest-hospitals/appointment-page-nearest-hospitals.component';
 import { AppointmentPageFooterComponent } from './appointment-page-footer/appointment-page-footer.component';
+import { AppointmentPageHeaderComponent } from './appointment-page-header/appointment-page-header.component';
+import { AppointmentPageNearestHospitalsComponent } from './appointment-page-nearest-hospitals/appointment-page-nearest-hospitals.component';
+import { CurrentAppointmentsComponent } from './current-appointments/current-appointments.component';
+import { PreviousAppointmentsComponent } from './previous-appointments/previous-appointments.component';
+import { SignInPageComponent } from './sign-in-page/sign-in-page.component';
+import { SignInPageLoginComponent } from './sign-in-page-login/sign-in-page-login.component';
+import { SignInPagePhotoComponent } from './sign-in-page-photo/sign-in-page-photo.component';
+import { FormsModule } from '@angular/forms';
+import { AppointmentsListComponent } from './appointments-list/appointments-list.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NzTabsModule } from 'ng-zorro-antd/tabs'; 
+import { NzButtonComponent } from 'ng-zorro-antd/button';
+import { NearestHospitalsComponent } from './nearest-hospitals/nearest-hospitals.component';
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignInPageComponent,
-    SignInPagePhotoComponent,
-    SignInPageLoginComponent,
     AppointmentPageComponent,
-    AppointmentPageHeaderComponent,
     AppointmentPageAppointmentsComponent,
+    AppointmentPageFooterComponent,
+    AppointmentPageHeaderComponent,
     AppointmentPageNearestHospitalsComponent,
-    AppointmentPageFooterComponent
+    CurrentAppointmentsComponent,
+    PreviousAppointmentsComponent,
+    SignInPageComponent,
+    SignInPageLoginComponent,
+    SignInPagePhotoComponent,
+    AppointmentsListComponent,
+    NearestHospitalsComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    FontAwesomeModule,
+    NzTabsModule,
+    NzButtonComponent
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    { provide: NZ_I18N, useValue: en_US },
+    provideAnimationsAsync(),
+    provideHttpClient()
   ],
   bootstrap: [AppComponent]
 })
